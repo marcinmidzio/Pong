2324a2f6dd28df585b0bda33d128c8a0b6916be0
user <user@CiechProj>
2016-03-17 22:49:34 +0100

    Klasa Pong

---
+++ src/pong/Pong.java
@@ -0,0 +1,68 @@
+package pong;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Random;
+
+import javax.swing.JFrame;
+import javax.swing.Timer;
+
+public class Pong implements ActionListener, KeyListener
+{
+
+	public static Pong pong;
+
+	public int width = 700, height = 700;
+
+	public Renderer renderer;
+
+	public Paddle player1;
+
+	public Paddle player2;
+
+	public Ball ball;
+
+	public boolean bot = false, selectingDifficulty;
+
+	public boolean w, s, up, down;
+
+	public int gameStatus = 0, scoreLimit = 7, playerWon; 
+
+	public int botDifficulty, botMoves, botCooldown = 0;
+
+	public Random random;
+
+	public JFrame jframe;
+
+	public Pong()
+	{
+		Timer timer = new Timer(20, this);
+		random = new Random();
+
+		jframe = new JFrame("Pong");
+
+		renderer = new Renderer();
+
+		jframe.setSize(width + 15, height + 35);
+		jframe.setVisible(true);
+		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jframe.add(renderer);
+		jframe.addKeyListener(this);
+
+		timer.start();
+	}
+
+	public void start()
+	{
+		gameStatus = 2;
+		player1 = new Paddle(this, 1);
+		player2 = new Paddle(this, 2);
+		ball = new Ball(this);
+	}
