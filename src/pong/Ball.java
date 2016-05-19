746e3539c97a88f712991c53cc91d95478433d06
user <user@192.168.1.8>
2016-05-19 20:05:22 +0200

    Klasa Ball

--- src/pong/Ball.java
+++ src/pong/Ball.java
@@ -56,3 +56,85 @@ public class Ball
 				}
 			}
 		}
+                
+                if (checkCollision(paddle1) == 1)
+		{
+			this.motionX = 1 + (amountOfHits / 5);
+			this.motionY = -2 + random.nextInt(4);
+
+			if (motionY == 0)
+			{
+				motionY = 1;
+			}
+
+			amountOfHits++;
+		}
+		else if (checkCollision(paddle2) == 1)
+		{
+			this.motionX = -1 - (amountOfHits / 5);
+			this.motionY = -2 + random.nextInt(4);
+
+			if (motionY == 0)
+			{
+				motionY = 1;
+			}
+
+			amountOfHits++;
+		}
+
+		if (checkCollision(paddle1) == 2)
+		{
+			paddle2.score++;
+			spawn();
+		}
+		else if (checkCollision(paddle2) == 2)
+		{
+			paddle1.score++;
+			spawn();
+		}
+	}
+
+	public void spawn()
+	{
+		this.amountOfHits = 0;
+		this.x = pong.width / 2 - this.width / 2;
+		this.y = pong.height / 2 - this.height / 2;
+
+		this.motionY = -2 + random.nextInt(4);
+
+		if (motionY == 0)
+		{
+			motionY = 1;
+		}
+
+		if (random.nextBoolean())
+		{
+			motionX = 1;
+		}
+		else
+		{
+			motionX = -1;
+		}
+	}
+
+	public int checkCollision(Paddle paddle)
+	{
+		if (this.x < paddle.x + paddle.width && this.x + width > paddle.x && this.y < paddle.y + paddle.height && this.y + height > paddle.y)
+		{
+			return 1; //bounce
+		}
+		else if ((paddle.x > x && paddle.paddleNumber == 1) || (paddle.x < x - width && paddle.paddleNumber == 2))
+		{
+			return 2; //score
+		}
+
+		return 0; //nothing
+	}
+
+	public void render(Graphics g)
+	{
+		g.setColor(Color.WHITE);
+		g.fillOval(x, y, width, height);
+	}
+
+}
