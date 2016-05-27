367015dd3c4c128cc5d00f55af9a6b1bff2d43b3
user <user@CiechProj>
2016-05-27 19:03:59 +0200

    Klasa Renderer

--- src/pong/Renderer.java
+++ src/pong/Renderer.java
@@ -4,3 +4,17 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
+public class Renderer extends JPanel
+{
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+
+		Pong.pong.render((Graphics2D) g);
+	}
+
+}
